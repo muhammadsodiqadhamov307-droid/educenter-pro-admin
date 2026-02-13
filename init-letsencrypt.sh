bd30196491a78ec5973f12bb5cf3776e55ef342c
FastTyper <fasttyper@example.com>
2026-02-13 16:23:48 +0500

    Configure for registrationadmin.duckdns.org

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.com www.example.com)
+domains=(registrationadmin.duckdns.org)
 rsa_key_size=4096
 data_path="./certbot"
 email="" # Adding a valid address is strongly recommended
